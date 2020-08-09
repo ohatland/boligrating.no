07a6dae00631df2e40b76bef208aa4ce8e24c3d0
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-08-09 22:52:31 +0200

    installed bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
+// app instance
 new Vue({
   render: h => h(App),
 }).$mount('#app')
