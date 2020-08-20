9356bea39130826620beea2661c10cfb84f2bde2
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-08-20 17:40:00 +0200

    get all example data

--- pages/api/address.js
+++ pages/api/address.js
@@ -6,7 +6,7 @@ FROM adresser
 LEFT JOIN reviews
 ON adresser.bruksenhetid = reviews.adresse_id
 WHERE MATCH(veinavn, nummer_bokstav, poststed, kommunenavn) 
-AGAINST('+skurkebakken' IN BOOLEAN MODE) ORDER BY veinavn, nummer;
+AGAINST('+Andeby' IN BOOLEAN MODE) ORDER BY veinavn, nummer;
 `
 
 export default (req, res) => {
