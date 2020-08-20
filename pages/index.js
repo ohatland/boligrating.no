6e200616404488074da671535d4df9dd8e777341
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-08-20 18:02:47 +0200

    installed talwind css framework

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>Boligrating</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏠</text></svg>" />
       </Head>
-      <h1>Hello test</h1>
+      <h1 className="text-6xl font-semibold italic">Hello test</h1>
       <Searchbar />
     </div>
   )
