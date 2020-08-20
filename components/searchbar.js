b2e23f5cd24139b2b8665024c0c212824849cf4e
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-08-20 23:58:21 +0200

    added search icon to button

--- components/searchbar.js
+++ components/searchbar.js
@@ -2,9 +2,11 @@ export default function Searchbar() {
     return (
         <div className="content-center p-8 lg:w-1/2">
             <form action="/search" method="get" className="bg-white shadow-md rounded">
-                <div className="flex items-center p-1 w-full">
-                    <input className="p-3 w-full" placeholder="Skurkebakken 1 Andeby" name="address" />
-                    <button className="align-right p-3" type="submit">Søk</button>
+                <div className="flex items-center p-1 w-full ">
+                    <input className="p-3 w-full text-green-700" placeholder="Skurkebakken 1 Andeby" name="address" />
+                    <button className="align-right p-3 h-12 w-12" type="submit">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 111.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 108 2a6 6 0 000 12z"/></svg>
+                    </button>
                 </div>
             </form>
         </div>
