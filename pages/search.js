2bbe16337c3bdaae7d4bde8cfd949164dc41f82d
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-08-20 22:11:53 +0200

    new design and implemented fetch from internal api

--- pages/search.js
+++ pages/search.js
@@ -1,38 +1,33 @@
 import { useRouter } from 'next/router'
 import useSWR from 'swr'
+import fetch from '../libs/fetch'
+import Searchbar from '../components/searchbar'
+import Header from '../components/header'
 
 export default function Search({ karverketAdresser }) {
-    const router = useRouter()
-    const { address } = router.query
+    const { address } = useRouter().query
+    const { data, error } = useSWR('/api/address', fetch)
+    
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+
     return (
-        <>
-            <h1>Du har søkt på: {address}</h1>
-            <h1>Karverket sier</h1>
+        <div>
+            <Header />
+            <Searchbar />
             <ul>
-                {karverketAdresser.map(({ adressetekst, postnummer, poststed }) => (
-                    <li>
-                        <h4>{adressetekst}</h4>
-                        <p>{postnummer}</p>
-                        <p>{poststed}</p>
-                    </li>
+                <ul>
+                {data.map((address) => (
+                        <li key={address.bruksenhetid}>
+                            <div className="w-4/6 bg-gray-200 p-1 m-1">
+                                <h4>{address.veinavn}</h4>
+                                <p>{address.nummer}</p>
+                                <p>{address.poststed}</p>
+                            </div>
+                        </li>                    
                 ))}
             </ul>
-        </>
+            </ul>
+        </div>
     )
-}
-
-// This gets called on every request
-export async function getServerSideProps(context) {
-    // Get query from submited from
-    const query = context.query.address
-
-    // Fetch address from Firebase API
-    
-
-    // Pass data to the page via props
-    return {
-        props: {
-            karverketAdresser
-        }
-    }
 }
