08441136bc9beb4a4921e3111782d891776750cc
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-08-21 18:29:58 +0200

    experimenting with layouts

--- pages/search.js
+++ pages/search.js
@@ -5,7 +5,9 @@ import Searchbar from '../components/searchbar'
 import Header from '../components/header'
 
 export default function Search({ karverketAdresser }) {
+    // get query from searchbar
     const { address } = useRouter().query
+
     const { data, error } = useSWR('/api/address', fetch)
     
     if (error) return <div>failed to load</div>
@@ -14,17 +16,18 @@ export default function Search({ karverketAdresser }) {
     return (
         <div>
             <Header />
-            <Searchbar />
+            <Searchbar query={address} />
             <ul>
                 <ul>
                 {data.map((address) => (
-                        <li key={address.bruksenhetid}>
-                            <div className="w-4/6 bg-gray-200 p-1 m-1">
-                                <h4>{address.veinavn}</h4>
-                                <p>{address.nummer}</p>
-                                <p>{address.poststed}</p>
-                            </div>
-                        </li>                    
+                    <li key={address.bruksenhetid}>
+                        <div className="flex w-auto bg-gray-200 p-3 m-8 shadow-lg rounded-lg">
+                            <p className="flex-1">{address.karakter_total}</p>
+                            <p className="flex-1">{address.veinavn}</p>
+                            <p className="flex-1">{address.nummer_bokstav}</p>
+                            <p className="flex-1 text-pink-500">{address.poststed}</p>
+                        </div>
+                    </li>                    
                 ))}
             </ul>
             </ul>
