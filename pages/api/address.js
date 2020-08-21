160b5813050e4c04305c2f0c51295b1d12a18a2e
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-08-21 18:29:41 +0200

    exmerimenting with querys

--- pages/api/address.js
+++ pages/api/address.js
@@ -6,9 +6,9 @@ FROM adresser
 LEFT JOIN reviews
 ON adresser.bruksenhetid = reviews.adresse_id
 WHERE MATCH(veinavn, nummer_bokstav, poststed, kommunenavn) 
-AGAINST('+Andeby' IN BOOLEAN MODE) 
-ORDER BY veinavn, nummer
-LIMIT 5;
+AGAINST('+Byvegen+5' IN BOOLEAN MODE) 
+ORDER BY veinavn, nummer, nummer_bokstav
+LIMIT 10;
 `
 
 export default (req, res) => {
