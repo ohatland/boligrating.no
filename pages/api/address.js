eb4277eeb50514479b439b12b3b73199666da1ff
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-08-22 03:46:35 +0200

    better response with empty query

--- pages/api/address.js
+++ pages/api/address.js
@@ -1,4 +1,4 @@
-import { pool } from '../../libs/database'
+import pool from '../../libs/database'
 
 const sql = `
 SELECT * 
@@ -9,16 +9,53 @@ ORDER BY veinavn, nummer, nummer_bokstav
 LIMIT 7;
 `
 
-export default (req, res) => {
-    // TODO - serialize input
-    // inserts '+' to work with BOOLEAN search mode in mysql
-    const search = '+' + req.query.search.split(' ').join('+')
-    
+export default async (req, res) => {
+
+    console.log("API fikk følgende query",req.query)
+
+    if (req.query.q === 'undefined') {
+        emptyResponse(res)
+    }
+
+    else if (req.query.q) {
+        
+        // TODO - serialize input
+        // inserts '+' to work with BOOLEAN search mode in mysql
+        const searchstring = '+' + req.query.q.split(' ').join('+')
+        
         await pool.query(sql, [searchstring], (err, address, fields) => {
             if (err) {
                 console.error(err)
-        }
-        res.statusCode = 200
-        res.json(address)
-    })
-  }
+
+                console.log("API response: KRISE!")
+                res.statusCode = 500
+                res.json({ message: 'KRISE!' })
+            }
+
+
+            else if (address.length == 0) {
+                emptyResponse(res)
+            }
+
+            else {
+                console.log("API response: OK")
+                res.statusCode = 200
+                res.json({
+                    message: "OK",
+                    address: address
+                })
+            }
+        })
+        
+    } 
+    
+    else {
+        emptyResponse(res)
+    }
+}
+
+function emptyResponse(res) {
+    console.log("API response: Empty searchstring")
+    res.statusCode = 200
+    res.json({ message: "Empty searchstring"})
+}
