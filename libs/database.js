18cb70f1bbda167a08e1b8ca0a4b21496b5242dc
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-09-01 21:41:02 +0200

    moved sensetive valuse to dotenv file

--- libs/database.js
+++ libs/database.js
@@ -1,12 +1,17 @@
 import mysql from 'mysql'
 import util from 'util'
+import dotenv from 'dotenv'
+
+// load values from .env file
+dotenv.config()
 
 let pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'boligrating_testdata'
+    host: process.env.DB_IP,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: 'boligrating'
 })
 
 pool.query = util.promisify(pool.query)
