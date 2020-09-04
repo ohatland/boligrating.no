1ce53126e702918bc1ac12fde86b6788ef1129a9
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-09-04 03:52:32 +0200

    uses new sp from db, gets all info in one query

--- pages/api/address.js
+++ pages/api/address.js
@@ -1,9 +1,6 @@
 import pool from '../../libs/database'
 
-const sqlAdresser = 'call finn_adresser(?)'
-const sqlLeiligheter = 'call finn_leiligheter_med_adresse_id(?)'
-const sqlReviews = 'call finn_reviews_med_adresse_id(?)'
-
+const sqlAdresser = 'call finn_adresser_med_navn(?)'
 export default async (req, res) => {
 
     // if api recives a query with id q
@@ -17,21 +14,15 @@ export default async (req, res) => {
         // inserts '+' before every word in searchstring to work with BOOLEAN search mode in mysql
         const searchstring = '+' + req.query.q.split(' ').join('+')
 
-        data.adresser = (await pool.query(sqlAdresser, [searchstring]))[0][0]
+        const [rows, fields] = await pool.query(sqlAdresser, [searchstring])
+        data.adresser = rows[0]
 
         if (data.adresser.length == 0) {
             emptyResponse(res, data)
         }
-
-        else {   
-            for (const adresse of data.adresser) {
-                adresse.leiligheter = (await pool.query(sqlLeiligheter, [adresse.id]))[0][0]
-                adresse.reviews = (await pool.query(sqlReviews, [adresse.id]))[0][0]
-            }
             
-            res.statusCode = 200
-            res.json(data)
-        }
+        res.statusCode = 200
+        res.json(data)
     }
 
     else {
