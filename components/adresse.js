287dff7cbbf164675e05f644ce6faa9fbf5ab27e
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-09-05 18:46:21 +0200

    moved component and use own css

--- components/adresse.js
+++ components/adresse.js
@@ -2,27 +2,27 @@ export default function Adresse({ adresse }) {
 
     const arr = [1, 2, 3, 4, 5]
     return (
-        <div className="adresse">
-            <div className="flex-1">
-                <div className="flex flex-row pt-1">
-                    {arr.map((num) => {
-                        if (adresse.snitt_karakter >= num - 0.5) { return (<i class="fas fa-star h-4"></i>) }
-                        else if (adresse.snitt_karakter >= num - 1 && num - adresse.snitt_karakter < 1) { return (<i class="fas fa-star-half-alt h-4"></i>) }
-                        else { return (<i class="far fa-star h-4"></i>) }
-                    })}
+            <div className="adresse">
+                <div className="vurderinger">
+                    <div className="stjerner">
+                        {arr.map((num) => {
+                            if (adresse.snitt_karakter >= num - 0.5) { return (<i class="fas fa-star h-4"></i>) }
+                            else if (adresse.snitt_karakter >= num - 1 && num - adresse.snitt_karakter < 1) { return (<i class="fas fa-star-half-alt h-4"></i>) }
+                            else { return (<i class="far fa-star h-4"></i>) }
+                        })}
+                    </div>
+                    <p className="text-xs">{adresse.antall_reviews} vurderinger</p>
                 </div>
-                <p>{adresse.antall_reviews} vurderinger</p>
+                <div className="adresseText">
+                        <p>{adresse.veinavn} {adresse.bolignummer}</p>
+                        <p className="text-xs capitalize">{(adresse.poststed).toLowerCase()}</p>
+                </div>
+                <div className="stedsnavn">
+                    <p>{adresse.kommunenavn}</p>
+                </div>
             </div>
-            <div className="flex-1">
-                <p>{adresse.veinavn} {adresse.bolignummer}</p>
-                <p className="text-sm capitalize">{(adresse.poststed).toLowerCase()}</p>
-            </div>
-            <div className="flex-1">
-                <p>{adresse.kommunenavn}</p>
-            </div>
-        </div>
     )
-} 
+}
 
 function fixPostName(name) {
     return name.toLowerCase()
--- pages/search.js
+++ pages/search.js
@@ -8,7 +8,7 @@ export default function Search() {
     const { query } = useRouter()
     const fetcher = (...args) => fetch(...args).then(res => res.json())
     const { data, error } = useSWR(`/api/address?q=${query.address}`, fetcher)
-
+    const arr = [1, 2, 3, 4, 5]
     if (!data) {
         return (<div></div>)
     }
@@ -21,12 +21,28 @@ export default function Search() {
         return (
             <div>
                 <Searchbar query={query.address} />
-                <ul>
+                <ul className="searchtable">
                     {data.adresser.map((adresse) => (
-                        <li key={adresse.id}>
+                        <li key={adresse.id} className="searchresult">
                             <Link href="/adresse/[id]" as={`/adresse/${adresse.id}`}>
-                                <div className="my-2">
-                                    <Adresse adresse={adresse} />
+                                <div className="adresse">
+                                    <div className="vurderinger">
+                                        <div className="stjerner">
+                                            {arr.map((num) => {
+                                                if (adresse.snitt_karakter >= num - 0.5) { return (<i class="fas fa-star h-4"></i>) }
+                                                else if (adresse.snitt_karakter >= num - 1 && num - adresse.snitt_karakter < 1) { return (<i class="fas fa-star-half-alt h-4"></i>) }
+                                                else { return (<i class="far fa-star h-4"></i>) }
+                                            })}
+                                        </div>
+                                        <p className="text-xs">{adresse.antall_reviews} vurderinger</p>
+                                    </div>
+                                    <div className="adresseText">
+                                        <p>{adresse.veinavn} {adresse.bolignummer}</p>
+                                        <p className="text-xs capitalize">{(adresse.poststed).toLowerCase()}</p>
+                                    </div>
+                                    <div className="stedsnavn">
+                                        <p>{adresse.kommunenavn}</p>
+                                    </div>
                                 </div>
                             </Link>
                         </li>
@@ -44,3 +60,7 @@ export default function Search() {
     )
 }
 
+
+function fixPostName(name) {
+    return name.toLowerCase()
+}
