8ee51efc9b139d498294ee13ec368ed837961244
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-09-05 05:22:50 +0200

    added footer component

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import '../styles/talwind.css'
 import '../styles/global.css'
 import '../styles/footer.css'
 import Header from '../components/header'
+import Footer from '../components/footer'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   )
 }
