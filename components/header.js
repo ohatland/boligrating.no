2cee6abbc3acf867c924bdcb3a6a2e85a6b51527
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-09-05 05:20:33 +0200

    import font awesome icons

--- components/header.js
+++ components/header.js
@@ -5,6 +5,7 @@ export default function Header() {
           <Head>
             <title>Boligrating.no</title>
             <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏠</text></svg>" />
+            <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js" integrity="sha384-3Nqiqht3ZZEO8FKj7GR1upiI385J92VwWNLj+FqHxtLYxd9l+WYpeqSOrLh0T12c" crossorigin="anonymous"></script>
           </Head>
       )
 }
