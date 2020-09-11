8911525af94d489a2abe729a6c529e674cefd125
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-09-11 04:57:08 +0200

    component for displaying 5 stars

---
+++ components/stjerner.js
@@ -0,0 +1,16 @@
+import style from '../styles/stjerner.module.css'
+export default function Stjerner({ karakter }) {
+    const arr = [1, 2, 3, 4, 5]
+    return (
+            <div className={style.stjerner}>
+                {arr.map((num) => {
+                    if (Math.round(karakter) >= num) { 
+                        return <svg key={num} className={style.stjerneja} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/></svg>
+                    }
+                    else { 
+                        return <svg key={num} className={style.stjernenei} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"/></svg>
+                    }
+                })}
+            </div>
+    )
+}
