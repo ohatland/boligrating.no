e8c861502539b98b368d6c4ce16c7be261660b34
Øyvind Hatland <oyvind.hatland@yahoo.com>
2020-09-11 04:58:21 +0200

    removeing unused api pages

--- pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-  
-  // Fungerer dårlig
-  //res.json({ name: 'Øyvind'})
-}
